rjmorris/cursewords

commit c9acb8f6f24d464f9499d4b7e282bea2c5eedf86

Use parens to encircle letters
Sometimes a puzzle circles letters, using the circled letters as an
additional twist. A circled letter had been represented on the board by
a separate Unicode symbol showing the letter inside a circle. My
combination of terminal and fonts had trouble rendering these Unicode
symbols. To get around this, I added parentheses around the letter
instead.

The parentheses go in the "markup" area in the cell. The markup area is
a single character before and after the actual letter. Usually it's
filled with a space. Previously, a colon was added to this area to
indicated a revealed letter, and a dot was added to indicate a corrected
letter. I had to remove these to accommodate the parentheses. This is OK
for me, though, because I don't really care about revealed/corrected
letters.

cursewords/cursewords.py
@@ -282,7 +282,7 @@ class Grid:
 
     def to_term(self, position):
         point_x, point_y = position
-        term_x = self.grid_x + (4 * point_x) + 2
+        term_x = self.grid_x + (4 * point_x) + 1
         term_y = self.grid_y + (2 * point_y) + 1
         return (term_y, term_x)
 
@@ -314,35 +314,32 @@ class Grid:
         else:
             value = cell.entry
 
-        if cell.circled:
-            value = encircle(value)
-
         if cell.marked_wrong:
             value = self.term.red(value.lower())
 
-        markup = ' '
+        pre_markup = ' '
+        post_markup = ' '
 
-        if cell.corrected:
-            markup = self.term.red(".")
-        if cell.revealed:
-            markup = self.term.red(":")
+        if cell.circled:
+            pre_markup = "("
+            post_markup = ")"
 
-        return value, markup
+        return pre_markup, value, post_markup
 
     def draw_cell(self, position):
-        value, markup = self.compile_cell(position)
-        value += markup
-        print(self.term.move(*self.to_term(position)) + value)
+        pre_markup, value, post_markup = self.compile_cell(position)
+        cell_value = pre_markup + value + post_markup
+        print(self.term.move(*self.to_term(position)) + cell_value)
 
     def draw_highlighted_cell(self, position):
-        value, markup = self.compile_cell(position)
-        value = self.term.cyan_underline(value) + markup
-        print(self.term.move(*self.to_term(position)) + value)
+        pre_markup, value, post_markup = self.compile_cell(position)
+        cell_value = pre_markup + self.term.cyan_underline(value) + post_markup
+        print(self.term.move(*self.to_term(position)) + cell_value)
 
     def draw_cursor_cell(self, position):
-        value, markup = self.compile_cell(position)
-        value = self.term.cyan_reverse(value) + markup
-        print(self.term.move(*self.to_term(position)) + value)
+        pre_markup, value, post_markup = self.compile_cell(position)
+        cell_value = pre_markup + self.term.cyan_reverse(value) + post_markup
+        print(self.term.move(*self.to_term(position)) + cell_value)
 
     def get_notification_input(self, message, timeout=5, chars=3,
             input_condition=str.isalnum, blocking=False):
@@ -669,14 +666,6 @@ def small_nums(number):
         small_num += num_dict[digit]
 
     return small_num
-
-def encircle(letter):
-    circle_dict = {"A": "Ⓐ", "B": "Ⓑ", "C": "Ⓒ", "D": "Ⓓ", "E": "Ⓔ", "F": "Ⓕ",
-                   "G": "Ⓖ", "H": "Ⓗ", "I": "Ⓘ", "J": "Ⓙ", "K": "Ⓚ", "L": "Ⓛ",
-                   "M": "Ⓜ", "N": "Ⓝ", "O": "Ⓞ", "P": "Ⓟ", "Q": "Ⓠ", "R": "Ⓡ",
-                   "S": "Ⓢ", "T": "Ⓣ", "U": "Ⓤ", "V": "Ⓥ", "W": "Ⓦ", "X": "Ⓧ",
-                   "Y": "Ⓨ", "Z": "Ⓩ", " ": "◯"}
-    return circle_dict[letter]
 
 
 def main():

commit 67ac6e6328e9561fdc3de37ece012325d00ca83b

Fix placement of blocks and numbers
The placement was broken in commit c9acb8f. The blocks and numbers were
placed 1 character to left of where they should be.

cursewords/cursewords.py
@@ -182,12 +182,11 @@ class Grid:
             if cell.is_letter():
                 self.draw_cell(position)
             elif cell.is_block():
-                print(self.term.move(y_coord, x_coord - 1) + chars.squareblock)
+                print(self.term.move(y_coord, x_coord) + chars.squareblock)
 
             if cell.number:
                 small = small_nums(cell.number)
-                x_pos = x_coord - 1
-                print(self.term.move(y_coord - 1, x_pos) + small)
+                print(self.term.move(y_coord - 1, x_coord) + small)
 
         return None
 